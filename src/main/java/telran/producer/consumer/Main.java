678bb46310997651434df14b661e21c5b94ba54f
Alexander Yavtuhovich <yavtik@gmail.com>
2024-12-15 20:17:31 +0200

    solution with getName() and setMessageBox()

--- src/main/java/telran/producer/consumer/Main.java
+++ src/main/java/telran/producer/consumer/Main.java
@@ -1,21 +1,35 @@
 package telran.producer.consumer;
 
 public class Main {
-    private static final int N_MESSAGES = 40;
+    private static final int N_MESSAGES = 20;
     private static final int N_RECEIVERS = 10;
 
     public static void main(String[] args) throws InterruptedException {
         MessageBox evenMessageBox = new SimpleMessageBox();
         MessageBox oddMessageBox = new SimpleMessageBox();
         Sender sender = new Sender(N_MESSAGES, evenMessageBox, oddMessageBox);
+        Receiver[] receivers = new Receiver[N_RECEIVERS];
 
         for (int i = 0; i < N_RECEIVERS; i++) {
-            new Receiver((i % 2 == 0) ? oddMessageBox : evenMessageBox).start();
+            receivers[i] = new Receiver(null);
+            if (isReceiverEven(receivers[i])) {
+                receivers[i].setMessageBox(evenMessageBox);
+            } else {
+                receivers[i].setMessageBox(oddMessageBox);
+            }
+        }
+
+        for (Receiver receiver : receivers) {
+            receiver.start();
         }
 
         sender.start();
         sender.join();
 
         Thread.sleep(100);
+    }
+
+    private static boolean isReceiverEven(Receiver receiver) {
+        return Integer.parseInt(receiver.getName().split("-")[1]) % 2 == 0;
     }
 }
